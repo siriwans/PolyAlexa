a2958591c88221f99f88680cd3f050764f7deb49
siriwans <35212476+siriwans@users.noreply.github.com>
2020-11-13 03:24:02 -0800

    deploy

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -6,13 +6,13 @@
 # This sample is built using the handler classes approach in skill builder.
 import logging
 from ask_sdk_core.skill_builder import SkillBuilder
-from intents import LaunchRequestHandler as launch
-from intents import QuestionIntentHandler as question
-from intents import HelpIntentHandler as help
-from intents import CancelOrStopIntentHandler as cancel
-from intents import SessionEndedRequestHandler as session
-from intents import IntentReflectorHandler as reflector
-from errors import CatchAllExceptionHandler as errors
+from .intents import LaunchRequestHandler as launch
+from .intents import QuestionIntentHandler as question
+from .intents import HelpIntentHandler as help
+from .intents import CancelOrStopIntentHandler as cancel
+from .intents import SessionEndedRequestHandler as session
+from .intents import IntentReflectorHandler as reflector
+from .errors import CatchAllExceptionHandler as errors
 
 # The SkillBuilder object acts as the entry point for your skill, routing all request and response
 # payloads to the handlers above. Make sure any new handlers or interceptors you've
@@ -32,4 +32,4 @@ sb.add_request_handler(session.SessionEndedRequestHandler())
 sb.add_request_handler(reflector.IntentReflectorHandler())
 sb.add_exception_handler(errors.CatchAllExceptionHandler())
 
-lambda_handler = sb.lambda_handler()
+application = sb.lambda_handler()
