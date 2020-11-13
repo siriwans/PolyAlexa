a5331aec1fd0f68a9362d802461d7435006013af
siriwans <35212476+siriwans@users.noreply.github.com>
2020-11-12 21:50:35 -0800

    changed from using lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -1,141 +1,35 @@
-# --------------- Helpers that build all of the responses ----------------------
+# -*- coding: utf-8 -*-
 
-def build_speechlet_response(output, reprompt_text, should_end_session):
-    return {
-        'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
-        },
-        'reprompt': {
-            'outputSpeech': {
-                'type': 'PlainText',
-                'text': reprompt_text
-            }
-        },
-        'shouldEndSession': should_end_session
-    }
+# This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK for Python.
+# Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+# session persistence, api calls, and more.
+# This sample is built using the handler classes approach in skill builder.
+import logging
+from ask_sdk_core.skill_builder import SkillBuilder
+from intents import LaunchRequestHandler as launch
+from intents import QuestionIntentHandler as question
+from intents import HelpIntentHandler as help
+from intents import CancelOrStopIntentHandler as cancel
+from intents import SessionEndedRequestHandler as session
+from intents import IntentReflectorHandler as reflector
+from errors import CatchAllExceptionHandler as errors
 
-def build_response(session_attributes, speechlet_response):
-    return {
-        'version': '1.0',
-        'sessionAttributes': session_attributes,
-        'response': speechlet_response
-    }
+# The SkillBuilder object acts as the entry point for your skill, routing all request and response
+# payloads to the handlers above. Make sure any new handlers or interceptors you've
+# defined are included below. The order matters - they're processed top to bottom.
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-# --------------- Functions that control the skill's behavior ------------------
-def get_question_response(intent):
-    """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
-    in your alexa skill in order for it to work.
-    """
-    session_attributes = {}
-    print(intent)
-    question = intent['slots']['question']['value']
-    
-    # TODO generate answer to question here.....
-    
-    speech_output = "The question is: " + question
-    reprompt_text = "Please say a question."
-    should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(
-        speech_output, reprompt_text, should_end_session))
+sb = SkillBuilder()
 
-def get_welcome_response():
-    """ If we wanted to initialize the session to have some attributes we could
-    add those here
-    """
-    session_attributes = {}
-    speech_output = "Welcome to PolyAlexa, you can now ask a questio about Cal Poly SLO."
-    
-    # If the user either does not reply to the welcome message or says something
-    # that is not understood, they will be prompted again with this text.
-    
-    reprompt_text = "I don't know if you heard me, welcome to PolyAlexa. You can now ask a question about Cal Poly SLO."
-    should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(
-        speech_output, reprompt_text, should_end_session))
+sb.add_request_handler(launch.LaunchRequestHandler())
+sb.add_request_handler(question.QuestionIntentHandler())
+sb.add_request_handler(help.HelpIntentHandler())
+sb.add_request_handler(cancel.CancelOrStopIntentHandler())
+sb.add_request_handler(session.SessionEndedRequestHandler())
+# make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+sb.add_request_handler(reflector.IntentReflectorHandler())
+sb.add_exception_handler(errors.CatchAllExceptionHandler())
 
-
-def handle_session_end_request():
-    speech_output = "Thank you for using PolyAlexa. Have a good day!"
-    
-    # Setting this to true ends the session and exits the skill.
-    should_end_session = True
-    return build_response({}, build_speechlet_response(
-        speech_output, None, should_end_session))
-
-# --------------- Events ------------------
-
-def on_session_started(session_started_request, session):
-    """ Called when the session starts.
-        One possible use of this function is to initialize specific 
-        variables from a previous state stored in an external database
-    """
-    # Add additional code here as needed
-    # if we need to store any variables the user ask about...
-    pass
-
-    
-
-def on_launch(launch_request, session):
-    """ Called when the user launches the skill without specifying what they
-    want
-    """
-    # Dispatch to your skill's launch message
-    return get_welcome_response()
-
-
-def on_intent(intent_request, session):
-    """ Called when the user specifies an intent for this skill """
-
-    intent = intent_request['intent']
-    intent_name = intent_request['intent']['name']
-
-    # Dispatch to your skill's intent handlers
-    if intent_name == "QuestionIntent":
-        return get_question_response(intent)
-    elif intent_name == "AMAZON.HelpIntent":
-        return get_welcome_response()
-    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
-        return handle_session_end_request()
-    else:
-        raise ValueError("Invalid intent")
-
-
-def on_session_ended(session_ended_request, session):
-    """ Called when the user ends the session.
-    Is not called when the skill returns should_end_session=true
-    """
-    print("on_session_ended requestId=" + session_ended_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
-    # add cleanup logic here
-
-
-# --------------- Main handler ------------------
-
-def lambda_handler(event, context):
-    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
-    etc.) The JSON body of the request is provided in the event parameter.
-    """
-    print("Incoming request...")
-
-    """
-    Uncomment this if statement and populate with your skill's application ID to
-    prevent someone else from configuring a skill that sends requests to this
-    function.
-    """
-    # if (event['session']['application']['applicationId'] !=
-    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
-    #     raise ValueError("Invalid Application ID")
-
-    if event['session']['new']:
-        on_session_started({'requestId': event['request']['requestId']},
-                           event['session'])
-
-    if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])
-    elif event['request']['type'] == "IntentRequest":
-        return on_intent(event['request'], event['session'])
-    elif event['request']['type'] == "SessionEndedRequest":
-        return on_session_ended(event['request'], event['session'])
-
+lambda_handler = sb.lambda_handler()
